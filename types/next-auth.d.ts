f059b3a129f42784d2745a3545890856638fa914
opsvantagedigi <ajay.sidal@opsvantagedigital.online>
2026-01-13 14:08:24 +1300

    fix(types): extend NextAuth types for user.id on session.user

---
+++ types/next-auth.d.ts
@@ -0,0 +1,12 @@
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+
+declare module "next-auth" {
+  interface User extends DefaultUser {
+    id: string;
+  }
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
