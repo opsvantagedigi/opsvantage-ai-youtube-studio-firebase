22bd96e6fd715f911fbb0cce8eb6be005c390417
ajay.sidal <ajay.sidal@opsvantagedigital.online>
2026-01-21 20:38:55 +0000

    fix: correct typo in RootLayout children type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.Node;
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
